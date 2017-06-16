4b1295906b6977e399c36497309656e0224ccd3e
Pranav Bokey <pbokey@gmail.com>
2017-06-16 00:03:33 -0400

    edited everything out to test if app still works

--- app.py
+++ app.py
@@ -40,11 +40,9 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    if (message_text == "previous close"):
-                        send_message(sender_id, "previous close")
-                        break
                     if re.search(r"(?i)previous|close", message_text) != None:
                         message_text = re.sub(r"(?i)previous|close", "", message_text)
+                        send_message(sender_id, message_text.upper())
                         message_text = message_text.upper()
                         stock = Share(message_text)
                         stock_price = stock.get_prev_close()
