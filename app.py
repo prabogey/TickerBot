5eae939d74dfb9dca590879a3d5a90312c2f6d6a
Pranav Bokey <pbokey@gmail.com>
2017-06-18 23:38:47 -0400

    added support for asking about yearly high

--- app.py
+++ app.py
@@ -66,6 +66,20 @@ def webhook():
                                 message_to_send = "The opening price for {} is {}".format(message_text, stock_price)
                         except:
                             message_to_send = "Could not recognize the symbol"
+                    elif re.search(r"(?i)high", message_text) != None:
+                        if re.search(r"(?i)year|52|52 wk|52 week", message_text) != None:
+                            stock_symb = re.sub(r"(?i)high|year|52|52 wk|52 week","", message_text)
+                            try:
+                                stock = Share(stock_symb.strip())
+                                log(stock_symb.strip())
+                                stock_price = stock.get_year_high()
+                                log(stock_price)
+                                if (stock_price == None):
+                                    message_to_send = "error"
+                                else:
+                                    message_to_send = "The 52 wk high for {} is {}".format(message_text, stock_price)
+                            except:
+                                message_to_send = "Could not recognize the symbol"
                     elif re.search(r"(?i)close", message_text) != None:
                         stock_symb = re.sub(r"(?i)previous|close|for", "", message_text)
                         try:
--- test.py
+++ test.py
@@ -2,8 +2,16 @@ from yahoo_finance import Share
 import re
 
 #aapl = Share('AAPL')
-x = input()
-message_text = re.sub(r"(?i)market|cap|capitilazation","", x)
-stock = Share(message_text.strip())
-print(stock.get_market_cap())
-
+message_text = input()
+if re.search(r"(?i)high", message_text) != None:
+    if re.search(r"(?i)year|52|52 wk|52 week", message_text) != None:
+        stock_symb = re.sub(r"(?i)high|year|52|52 wk|52 week","", message_text)
+        stock = Share(stock_symb.strip())
+        stock_price = stock.get_year_high()
+        print(stock_price)
+        if (stock_price == None):
+            message_to_send = "error"
+        else:
+            message_to_send = "The 52 wk high for {} is {}".format(message_text, stock_price)
+#stock = Share(message_text.strip())
+#print(stock.get_market_cap())
